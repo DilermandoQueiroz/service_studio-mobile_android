05b9ae7da024132f9b82d37338b5320803686868
rruyzz <rodolforuyz@gmail.com>
2022-09-24 15:35:41 -0300

    login activity

---
+++ feature/login/src/main/java/com/app/login/LoginActivity.kt
@@ -0,0 +1,16 @@
+package com.app.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.app.login.databinding.ActivityLoginBinding
+
+class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
